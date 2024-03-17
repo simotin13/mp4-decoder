bdb551b7685848de536d84557d430f1f20a7c24a
Hiroyuki Miyazaki <miyazaki@mcommit.com>
2024-03-17 15:57:36 +0900

    WIP reading avc1,avcC

--- mp4.cpp
+++ mp4.cpp
@@ -27,7 +27,11 @@ bool Mp4::ReadMp4Info(std::string filePath, MP4_INFO &mp4Info, std::string &errM
         return false;
     }
 
+    TKHD_BOX_INFO tkhdBoxInfo;
     MVHD_BOX_INFO mvhdBoxInfo;
+    STSD_BOX_INFO stsdBoxInfo;
+    AVC1_BOX_INFO avc1BoxInfo;
+    AVCC_BOX_INFO avccBoxInfo;
     size_t totalSize = st.st_size;
     size_t leftSize  = totalSize;
     uint64_t boxSize  = 0;
@@ -50,8 +54,66 @@ bool Mp4::ReadMp4Info(std::string filePath, MP4_INFO &mp4Info, std::string &errM
         std::cout << "TODO: type:[" << type << "] boxSize:[" << boxSize << "]" << std::endl;
         if (type == "moov")
         {
-            std::cout << "moov boxSize:[" << boxSize << "]" << std::endl;
+            // have child box
             leftSize -= readSize;
+        }
+        else if (type == "trak")
+        {
+            // have child box
+            leftSize -= readSize;
+        }
+        else if (type == "stbl")
+        {
+            // have child box
+            leftSize -= readSize;
+        }
+        else if (type == "mdia")
+        {
+            // have child box
+            leftSize -= readSize;
+        }
+        else if (type == "minf")
+        {
+            // have child box
+            leftSize -= readSize;
+        }
+        else if (type == "dinf")
+        {
+            // have child box
+            leftSize -= readSize;
+        }
+        else if (type == "stco")
+        {
+            // have child box
+            leftSize -= readSize;
+        }
+        else if (type == "stsc")
+        {
+            // have child box
+            leftSize -= readSize;
+        }
+        else if (type == "stsz")
+        {
+            // have child box
+            leftSize -= readSize;
+        }
+        else if (type == "stts")
+        {
+            // have child box
+            leftSize -= readSize;
+        }
+        else if (type == "tkhd")
+        {
+            uint64_t seekSize = boxSize - readSize;
+            result = readTkhdBox(ifs, readSize, tkhdBoxInfo);
+            if (!result)
+            {
+                result = false;
+                break;
+            }
+            seekSize -= readSize;
+            ifs.seekg(seekSize, std::ios_base::cur);
+            leftSize -= boxSize;
         }
         else if (type == "mvhd")
         {
@@ -65,6 +127,85 @@ bool Mp4::ReadMp4Info(std::string filePath, MP4_INFO &mp4Info, std::string &errM
             seekSize -= readSize;
             ifs.seekg(seekSize, std::ios_base::cur);
             leftSize -= boxSize;
+        }
+        else if (type == "stsd")
+        {
+            uint64_t seekSize = boxSize - readSize;
+            result = readStsdBox(ifs, readSize, stsdBoxInfo);
+            if (!result)
+            {
+                result = false;
+                break;
+            }
+            leftSize -= readSize;
+        }
+        else if (type == "mdat")
+        {
+            uint64_t seekSize = boxSize-readSize;
+
+            ifs.seekg(seekSize, std::ios_base::cur);
+            leftSize -= boxSize;
+        }
+        else if (type == "tkhd")
+        {
+            uint64_t seekSize = boxSize - readSize;
+            result = readTkhdBox(ifs, readSize, tkhdBoxInfo);
+            if (!result)
+            {
+                result = false;
+                break;
+            }
+            seekSize -= readSize;
+            ifs.seekg(seekSize, std::ios_base::cur);
+            leftSize -= boxSize;
+        }
+        else if (type == "mvhd")
+        {
+            uint64_t seekSize = boxSize - readSize;
+            result = readMvhdBox(ifs, readSize, mvhdBoxInfo);
+            if (!result)
+            {
+                result = false;
+                break;
+            }
+            seekSize -= readSize;
+            ifs.seekg(seekSize, std::ios_base::cur);
+            leftSize -= boxSize;
+        }
+        else if (type == "stsd")
+        {
+            uint64_t seekSize = boxSize - readSize;
+            result = readStsdBox(ifs, readSize, stsdBoxInfo);
+            if (!result)
+            {
+                result = false;
+                break;
+            }
+            seekSize -= readSize;
+            ifs.seekg(seekSize, std::ios_base::cur);
+            leftSize -= boxSize;
+        }
+        else if (type == "avc1")
+        {
+            uint64_t seekSize = boxSize - readSize;
+            result = readAvc1Box(ifs, readSize, avc1BoxInfo);
+            if (!result)
+            {
+                result = false;
+                break;
+            }
+            leftSize -= readSize;
+        }
+        else if (type == "avcC")
+        {
+            uint64_t seekSize = boxSize - readSize;
+            result = readAvccBox(ifs, boxSize, readSize, avccBoxInfo);
+            if (!result)
+            {
+                result = false;
+                break;
+            }
+            leftSize -= readSize;
         }
         else if (type == "mdat")
         {
@@ -94,6 +235,20 @@ bool Mp4::byteToUInt16Be(std::ifstream &ifs, uint16_t &value)
     value = 0;
     value += (uint16_t)buf[pos++] << 8;
     value += (uint16_t)buf[pos++];
+
+    return true;
+}
+
+bool Mp4::byteToUInt24Be(std::ifstream &ifs, uint32_t &value)
+{
+    std::vector<unsigned char> buf(3);
+    ifs.read((char *)buf.data(), 3);
+
+    uint32_t pos = 0;
+    value = 0;
+    value += (uint32_t)buf[pos++]   << 16;
+    value += (uint32_t)buf[pos++]   << 8;
+    value += (uint32_t)buf[pos++];
 
     return true;
 }
@@ -201,6 +356,166 @@ bool Mp4::readBoxHeader(std::ifstream &ifs, uint64_t &readSize, uint64_t &boxSiz
     return true;
 }
 
+bool Mp4::readTkhdBox(std::ifstream &ifs, uint64_t &readSize, TKHD_BOX_INFO &tkhdBoxInfo)
+{
+    std::vector<unsigned char> buf;
+    readSize = 0;
+
+    // version
+    ifs.read((char*)&(tkhdBoxInfo.version), 1);
+    readSize++;
+
+    // Flags
+    ifs.read((char *)(tkhdBoxInfo.flags), 3);
+    readSize += 3;
+
+    // creation time
+    bool result = byteToUInt32Be(ifs, tkhdBoxInfo.creationTime);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 4;
+
+    // modification time
+    result = byteToUInt32Be(ifs, tkhdBoxInfo.modificationTime);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 4;
+
+    // track id
+    result = byteToUInt32Be(ifs, tkhdBoxInfo.trackId);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 4;
+
+    // reserved
+    result = byteToUInt32Be(ifs, tkhdBoxInfo.reserved);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 4;
+
+    // duration
+    result = byteToUInt32Be(ifs, tkhdBoxInfo.duration);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 4;
+
+    // reserved2
+    ifs.read((char*)&(tkhdBoxInfo.reserved2), 8);
+    readSize += 8;
+
+    // layer
+    result = byteToInt16Be(ifs, tkhdBoxInfo.layer);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 2;
+
+    // alternate group
+    result = byteToInt16Be(ifs, tkhdBoxInfo.alternateGroup);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 2;
+    
+    // volume
+    result = byteToInt16Be(ifs, tkhdBoxInfo.volume);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 2;
+
+    // reserved3
+    result = byteToInt16Be(ifs, tkhdBoxInfo.reserved3);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 2;
+
+    // matrix
+    for (int i = 0; i < 9; i++)
+    {
+        result = byteToInt32Be(ifs, tkhdBoxInfo.matrix[i]);
+        if (!result)
+        {
+            return false;
+        }
+        readSize += 4;
+    }
+
+    // width
+    result = byteToUInt16Be(ifs, tkhdBoxInfo.width);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 2;
+
+    // width quality
+    result = byteToUInt16Be(ifs, tkhdBoxInfo.widthQuality);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 2;
+
+
+    // height
+    result = byteToUInt16Be(ifs, tkhdBoxInfo.height);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 2;
+
+    // height quality
+    result = byteToUInt16Be(ifs, tkhdBoxInfo.heightQuality);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 2;
+
+    return true;
+}
+
+bool Mp4::readStsdBox(std::ifstream &ifs, uint64_t &readSize, STSD_BOX_INFO &stsdBoxInfo)
+{
+    std::vector<unsigned char> buf;
+    readSize = 0;
+
+    // version
+    ifs.read((char*)&(stsdBoxInfo.version), 1);
+    readSize++;
+
+    // Flags
+    ifs.read((char *)(stsdBoxInfo.flags), 3);
+    readSize += 3;
+
+    // entry count
+    bool result = byteToUInt32Be(ifs, stsdBoxInfo.entryCount);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 4;
+
+    return true;
+}
+
 bool Mp4::readMvhdBox(std::ifstream& ifs, uint64_t &readSize, MVHD_BOX_INFO &mvhdBoxInfo)
 {
     std::vector<unsigned char> buf;
@@ -288,6 +603,264 @@ bool Mp4::readMvhdBox(std::ifstream& ifs, uint64_t &readSize, MVHD_BOX_INFO &mvh
         return false;
     }
     readSize += 4;
+
+    return true;
+}
+
+bool Mp4::readAvc1Box(std::ifstream &ifs, uint64_t &readSize, AVC1_BOX_INFO &avc1BoxInfo)
+{
+    std::vector<unsigned char> buf;
+    readSize = 0;
+
+    ifs.read((char *)(avc1BoxInfo.reserved), 6);
+    readSize += 6;
+
+    bool result = byteToUInt16Be(ifs, avc1BoxInfo.dataReferenceIndex);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 2;
+
+    // version
+    result = byteToUInt16Be(ifs, avc1BoxInfo.version);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 2;
+
+    // revision level
+    result = byteToUInt16Be(ifs, avc1BoxInfo.revisionLevel);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 2;
+
+    // vendor
+    result = byteToUInt32Be(ifs, avc1BoxInfo.vendor);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 4;
+
+    // temporal quality
+    result = byteToUInt32Be(ifs, avc1BoxInfo.temporalQuality);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 4;
+
+    // spatial quality
+    result = byteToUInt32Be(ifs, avc1BoxInfo.spatialQuality);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 4;
+
+    // width
+    result = byteToUInt16Be(ifs, avc1BoxInfo.width);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 2;
+
+    // height
+    result = byteToUInt16Be(ifs, avc1BoxInfo.height);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 2;
+
+    // horizontal resolution
+    result = byteToUInt32Be(ifs, avc1BoxInfo.horizontalResolution);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 4;
+
+    // vertical resolution
+    result = byteToUInt32Be(ifs, avc1BoxInfo.verticalResolution);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 4;
+
+    // data size
+    result = byteToUInt32Be(ifs, avc1BoxInfo.dataSize);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 4;
+
+    // frame count
+    result = byteToUInt16Be(ifs, avc1BoxInfo.frameCount);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 2;
+
+    // compressor name
+    ifs.read((char *)(avc1BoxInfo.compressorName), 32);
+    readSize += 32;
+
+    // depth
+    result = byteToUInt16Be(ifs, avc1BoxInfo.depth);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 2;
+
+    // color table id
+    result = byteToInt16Be(ifs, avc1BoxInfo.colorTableId);
+    if (!result)
+    {
+        return false;
+    }
+    readSize += 2;
+
+    return true;
+}
+
+bool Mp4::readAvccBox(std::ifstream &ifs, const uint64_t boxSize, uint64_t &readSize, AVCC_BOX_INFO &avccBoxInfo)
+{
+    std::vector<unsigned char> buf;
+    readSize = 0;
+
+    // configuration version
+    ifs.read((char*)&(avccBoxInfo.configurationVersion), 1);
+    readSize++;
+
+    // AVCProfileIndication
+    ifs.read((char*)&(avccBoxInfo.AVCProfileIndication), 1);
+    readSize++;
+
+    // profile compatibility
+    ifs.read((char*)&(avccBoxInfo.profileCompatibility), 1);
+    readSize++;
+
+    // AVCLevelIndication
+    ifs.read((char*)&(avccBoxInfo.AVCLevelIndication), 1);
+    readSize++;
+
+    // lengthSizeMinusOne
+    ifs.read((char*)&(avccBoxInfo.lengthSizeMinusOne), 1);
+    avccBoxInfo.lengthSizeMinusOne = 0x03 & avccBoxInfo.lengthSizeMinusOne;
+    readSize++;
+
+    // numOfSequenceParameterSets
+    ifs.read((char*)&(avccBoxInfo.numOfSequenceParameterSets), 1);
+    avccBoxInfo.numOfSequenceParameterSets = 0x1F & avccBoxInfo.numOfSequenceParameterSets;
+    readSize++;
+
+    avccBoxInfo.sequenceParameterSets.clear();
+    for (int i = 0; i < avccBoxInfo.numOfSequenceParameterSets; i++)
+    {
+        // sequenceParameterSetLength
+        SEQUENCE_PARAMETER_SET sps;
+        bool result = byteToUInt16Be(ifs, sps.sequenceParameterSetLength);
+        if (!result)
+        {
+            return false;
+        }
+        readSize += 2;
+
+        sps.nalUnit.clear();
+        for (int j = 0; j < sps.sequenceParameterSetLength; j++)
+        {
+            sps.nalUnit.push_back(ifs.get());
+            readSize++;
+        }
+        avccBoxInfo.sequenceParameterSets.push_back(sps);
+    }
+
+    // numOfPictureParameterSets
+    ifs.read((char*)&(avccBoxInfo.numOfPictureParameterSets), 1);
+    readSize++;
+
+    avccBoxInfo.pictureParameterSets.clear();
+    for (int i = 0; i < avccBoxInfo.numOfPictureParameterSets; i++)
+    {
+        // pictureParameterSetLength
+        PICTURE_PARAMETER_SET pps;
+        bool result = byteToUInt16Be(ifs, pps.pictureParameterSetLength);
+        if (!result)
+        {
+            return false;
+        }
+        readSize += 2;
+
+        pps.nalUnit.clear();
+        for (int j = 0; j < pps.pictureParameterSetLength; j++)
+        {
+            pps.nalUnit.push_back(ifs.get());
+            readSize++;
+        }
+        avccBoxInfo.pictureParameterSets.push_back(pps);
+    }
+
+    avccBoxInfo.chromaFormat = -1;
+    avccBoxInfo.bitDepthLumaMinus8 = -1;
+    avccBoxInfo.bitDepthChromaMinus8 = -1;
+    if (0 < (boxSize - readSize))
+    {
+        if ((avccBoxInfo.AVCProfileIndication == AVC_PROFILE_INDICATION_H264_HIGH) ||
+            (avccBoxInfo.AVCProfileIndication == AVC_PROFILE_INDICATION_H264_HIGH_10) ||
+            (avccBoxInfo.AVCProfileIndication == AVC_PROFILE_INDICATION_H264_HIGH_422) ||
+            (avccBoxInfo.AVCProfileIndication == AVC_PROFILE_INDICATION_H264_HIGH_444))
+        {
+            // chromaFormat
+            avccBoxInfo.chromaFormat = ifs.get();
+            avccBoxInfo.chromaFormat = 0x03 & avccBoxInfo.chromaFormat;
+            readSize++;
+
+            // bitDepthLumaMinus8
+            avccBoxInfo.bitDepthLumaMinus8 = ifs.get();
+            avccBoxInfo.bitDepthLumaMinus8 = 0x07 & avccBoxInfo.bitDepthLumaMinus8;
+            readSize++;
+
+            // bitDepthChromaMinus8
+            avccBoxInfo.bitDepthChromaMinus8 = ifs.get();
+            avccBoxInfo.bitDepthChromaMinus8 = 0x07 & avccBoxInfo.bitDepthChromaMinus8;
+            readSize++;
+
+            // numOfSequenceParameterSetExt
+            avccBoxInfo.numOfSequenceParameterSetExt = ifs.get();
+            readSize++;
+
+            avccBoxInfo.sequenceParameterSetExts.clear();
+            for (int i = 0; i < avccBoxInfo.numOfSequenceParameterSetExt; i++)
+            {
+                // sequenceParameterSetExtLength
+                SEQUENCE_PARAMETER_SET sps;
+                bool result = byteToUInt16Be(ifs, sps.sequenceParameterSetLength);
+                if (!result)
+                {
+                    return false;
+                }
+                readSize += 2;
+
+                sps.nalUnit.clear();
+                for (int j = 0; j < sps.sequenceParameterSetLength; j++)
+                {
+                    sps.nalUnit.push_back(ifs.get());
+                    readSize++;
+                }
+                avccBoxInfo.sequenceParameterSetExts.push_back(sps);
+            }
+        }
+    }
 
     return true;
 }
--- mp4.h
+++ mp4.h
@@ -6,6 +6,14 @@
 #include <vector>
 #include <cstring>
 
+const uint8_t AVC_PROFILE_INDICATION_BASELINE = 0x42;
+const uint8_t AVC_PROFILE_INDICATION_H264_MAIN = 0x4D;
+const uint8_t AVC_PROFILE_INDICATION_H264_EXTENDED = 0x58;
+const uint8_t AVC_PROFILE_INDICATION_H264_HIGH = 0x64;
+const uint8_t AVC_PROFILE_INDICATION_H264_HIGH_10 = 0x6E;
+const uint8_t AVC_PROFILE_INDICATION_H264_HIGH_422 = 0x7A;
+const uint8_t AVC_PROFILE_INDICATION_H264_HIGH_444 = 0xF4;
+
 struct MP4_INFO
 {
     uint64_t recTimeSec;
@@ -13,21 +21,98 @@ struct MP4_INFO
     double fizeSizeMB;
 };
 
-// mvhd ボックスの情報
+struct TKHD_BOX_INFO
+{
+    uint8_t version;                        // varsion
+    uint8_t flags[3];                       // flags
+    uint32_t creationTime;                  // UTC Since 1904-01-01 00:00:00
+    uint32_t modificationTime;              // UTC Since 1904-01-01 00:00:00
+    uint32_t trackId;                       // track id
+    uint32_t reserved;                      // reserved
+    uint32_t duration;                      // time value that indicates the duration of the track in time scale units
+    uint8_t reserved2[8];                   // 8 reserved bytes (set to 0)
+    int16_t layer;                          // layer
+    int16_t alternateGroup;                 // alternate group
+    int16_t volume;                         // volume
+    int16_t reserved3;                      // reserved
+    int32_t matrix[9];                      // transformation matrix
+    uint16_t width;                         // width
+    uint16_t widthQuality;                  // width quality
+    uint16_t height;                        // height
+    uint16_t heightQuality;                 // height quality
+};
+
 struct MVHD_BOX_INFO
 {
-    uint8_t version;            // varsion
-    uint8_t flags[3];           // reverved for future
-    uint32_t creationTime;      // UTC Since 1904-01-01 00:00:00
-    uint32_t modificationTime;  // UTC Since 1904-01-01 00:00:00
-    uint32_t timeScale;         // second
-    uint32_t duration;          // time value that indicates the duration of the movie in time scale units
-    int32_t rate;               // preferred rate
-    int16_t volume;             // preferred volume
-    uint8_t reserved[10];       // 10 reserved bytes (set to 0)
-    int32_t matrix[9];          // transformation matrix
-    uint32_t preDefined[6];     // pre-defined
-    uint32_t nextTrackId;       // next track id
+    uint8_t version;                        // varsion
+    uint8_t flags[3];                       // flags
+    uint32_t creationTime;                  // UTC Since 1904-01-01 00:00:00
+    uint32_t modificationTime;              // UTC Since 1904-01-01 00:00:00
+    uint32_t timeScale;                     // second
+    uint32_t duration;                      // time value that indicates the duration of the movie in time scale units
+    int32_t rate;                           // preferred rate
+    int16_t volume;                         // preferred volume
+    uint8_t reserved[10];                   // 10 reserved bytes (set to 0)
+    int32_t matrix[9];                      // transformation matrix
+    uint32_t preDefined[6];                 // pre-defined
+    uint32_t nextTrackId;                   // next track id
+};
+
+struct STSD_BOX_INFO
+{
+    uint8_t version;                        // varsion
+    uint8_t flags[3];                       // flags
+    uint32_t entryCount;                    // entry count
+};
+
+struct AVC1_BOX_INFO
+{
+    uint8_t reserved[6];                    // 6 reserved bytes (set to 0)
+    uint16_t dataReferenceIndex;            // data reference index
+    uint16_t version;                       // version
+    uint16_t revisionLevel;                 // revision level
+    uint32_t vendor;                        // vendor
+    uint32_t temporalQuality;               // temporal quality
+    uint32_t spatialQuality;                // spatial quality
+    uint16_t width;                         // width
+    uint16_t height;                        // height
+    uint32_t horizontalResolution;          // horizontal resolution
+    uint32_t verticalResolution;            // vertical resolution
+    uint32_t dataSize;                      // data size
+    uint16_t frameCount;                    // frame count
+    uint8_t compressorName[32];             // compressor name
+    uint16_t depth;                         // depth
+    int16_t colorTableId;                   // color table id
+};
+
+struct SEQUENCE_PARAMETER_SET
+{
+    uint16_t sequenceParameterSetLength;            // nal unit length
+    std::vector<uint8_t> nalUnit;                   // nal unit
+};
+
+struct PICTURE_PARAMETER_SET
+{
+    uint16_t pictureParameterSetLength;             // nal unit length
+    std::vector<uint8_t> nalUnit;                   // nal unit
+};
+
+struct AVCC_BOX_INFO
+{
+    uint8_t configurationVersion;                               // configuration version
+    uint8_t AVCProfileIndication;                               // AVC profile indication(0x42:BaseLine, 0x64:High)
+    uint8_t profileCompatibility;                               // profile compatibility
+    uint8_t AVCLevelIndication;                                 // AVC level indication
+    uint8_t lengthSizeMinusOne;                                 // length size minus one
+    uint8_t numOfSequenceParameterSets;                         // number of sequence parameter sets
+    std::vector<SEQUENCE_PARAMETER_SET> sequenceParameterSets;  // sequence parameter sets
+    uint8_t numOfPictureParameterSets;                          // number of picture parameter sets
+    std::vector<PICTURE_PARAMETER_SET> pictureParameterSets;    // picture parameter sets
+    uint8_t chromaFormat;                                       // chroma format
+    uint8_t bitDepthLumaMinus8;                                 // bit depth luma minus 8
+    uint8_t bitDepthChromaMinus8;                               // bit depth chroma minus 8
+    uint8_t numOfSequenceParameterSetExt;                       // number of sequence parameter set ext
+    std::vector<SEQUENCE_PARAMETER_SET> sequenceParameterSetExts;  // sequence parameter set ext
 };
 
 class Mp4
@@ -36,12 +121,17 @@ public:
     static bool ReadMp4Info(std::string filePath, MP4_INFO &mp4Info, std::string &errMsg);
 private:
     static bool byteToUInt16Be(std::ifstream &ifs, uint16_t &value);
+    static bool byteToUInt24Be(std::ifstream &ifs, uint32_t &value);
     static bool byteToUInt32Be(std::ifstream &ifs, uint32_t &value);
     static bool byteToUInt64Be(std::ifstream &ifs, uint64_t &value);
     static bool byteToInt16Be(std::ifstream &ifs, int16_t &value);
     static bool byteToInt32Be(std::ifstream &ifs, int32_t &value);
     static bool readBoxHeader(std::ifstream &ifs, uint64_t &readSize, uint64_t &boxSize, std::string &type, std::string &errMsg);
     static bool readMvhdBox(std::ifstream &ifs, uint64_t &readSize, MVHD_BOX_INFO &mvhdBoxInfo);
-
+    static bool readTkhdBox(std::ifstream &ifs, uint64_t &readSize, TKHD_BOX_INFO &tkhdBoxInfo);
+    static bool readStsdBox(std::ifstream &ifs, uint64_t &readSize, STSD_BOX_INFO &stsdBoxInfo);
+    static bool readAvc1Box(std::ifstream &ifs, uint64_t &readSize, AVC1_BOX_INFO &avc1BoxInfo);
+    static bool readAvccBox(std::ifstream &ifs, const uint64_t boxSize, uint64_t &readSize, AVCC_BOX_INFO &avccBoxInfo);
+    
 };
 #endif // _MP4_H_
